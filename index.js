a59f74ab86a601734efbdd4a681b845d127310c6
Liam McMains <liam@mcmains.net>
2016-12-12 00:44:22 -0600

    posts are pulling down from reddit now

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -10,13 +11,31 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
 });
 
-app.get('/reddit', function(request, response) {
-  response.render('pages/reddit');
+app.get('/reddit', function(request, res) {
+
+    //https://www.reddit.com/r/rocketleagueexchange/new.json?sort=new
+    var request = require('request');
+    request('https://www.reddit.com/r/rocketleagueexchange/new.json?sort=new', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var json = JSON.parse(body);
+
+            json.data.children.forEach(function(post) {
+                console.log(post.data.title);
+            });
+
+            //console.log(JSON.stringify(json.data.children));
+
+            res.render('pages/reddit', {posts: json.data.children});
+        }
+    });
+
+
+
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
