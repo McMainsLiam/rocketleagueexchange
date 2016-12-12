92c0223eda5bcb5207ea47f573145cdab07a2a55
Liam McMains <liam@mcmains.net>
2016-12-12 01:06:19 -0600

    added links to reddit

--- index.js
+++ index.js
@@ -14,6 +14,26 @@ app.get('/', function(request, response) {
     response.render('pages/index');
 });
 
+app.get('/reddit/xbox', function(request, res) {
+
+    //https://www.reddit.com/r/rocketleagueexchange/new.json?sort=new
+    var request = require('request');
+    request('https://www.reddit.com/r/rocketleagueexchange/new.json?sort=new', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var json = JSON.parse(body);
+
+            var posts = [];
+            json.data.children.forEach(function(post) {
+                if(post.data.title.indexOf("[Xbox]" !== -1)) {
+                    posts.push(post);
+                }
+            });
+
+            res.render('pages/reddit', {posts: posts});
+        }
+    });
+});
+
 app.get('/reddit', function(request, res) {
 
     //https://www.reddit.com/r/rocketleagueexchange/new.json?sort=new
