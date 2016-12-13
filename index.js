f2aa528d6c1d25b0a2a61b16ee331e5b8bfd711d
Liam McMains <liam@mcmains.net>
2016-12-13 00:22:38 -0600

    fixed not found page

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(app.router);
 
 app.get('/', function(request, response) {
     response.render('pages/index');
