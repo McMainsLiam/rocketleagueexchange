225de9ac935e2e245d982b20956699671f07d4e3
Liam McMains <liam@mcmains.net>
2016-12-13 00:11:50 -0600

    add refresh button

--- public/scripts/RetrieveExchangeData.js
+++ public/scripts/RetrieveExchangeData.js
@@ -24,6 +24,8 @@ function addDataToDOM(data) {
             document.getElementById('posts').innerHTML += '<div class="col-xs-12 col-sm-6 col-md-3"> <h5> <a href = ' +post.data.url + '>' +post.data.title + '</a> </h5> <h6> <a href = "https://www.reddit.com/u/' + post.data.author + '"><i>' +post.data.author + '</i></a> </h6> <a href="https://www.reddit.com/message/compose/?to=' + post.data.author+ '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-send"></span> Message </button> </a> <a href="' + post.data.url + '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-comment"></span> Comment </button> </a> </div>';
         }
     });
+
+    document.getElementById('refreshButton').innerHTML += '<button class = "btn btn-default wide" onclick="refreshPage();">  <span class="glyphicon glyphicon-refresh"></span> </button>';
 }
 
 function httpGetAsync(theUrl, callback)
@@ -35,4 +37,8 @@ function httpGetAsync(theUrl, callback)
     }
     xmlHttp.open("GET", theUrl, true); // true for asynchronous
     xmlHttp.send(null);
+}
+
+function refreshPage() {
+    window.location.reload();
 }
