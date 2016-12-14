c1e6aa74bd5335c409fcebe63078dfde2f21434b
Liam McMains <liam@mcmains.net>
2016-12-14 18:13:12 +0000

    add active tabs so users can see which one they are using

--- public/scripts/RetrieveExchangeData.js
+++ public/scripts/RetrieveExchangeData.js
@@ -1,8 +1,14 @@
 console.log("Up and running");
-console.log(window.location.href);
 httpGetAsync("https://www.reddit.com/r/rocketleagueexchange/new.json?sort=new", addDataToDOM);
 var mostRecentPosts = undefined;
 var hasFinishedLoading = false;
+
+function removeAllActiveClassesOnTabs() {
+    $('#xboxTab').removeClass('active');
+    $('#ps4Tab').removeClass('active');
+    $('#allTab').removeClass('active');
+    $('#pcTab').removeClass('active');
+}
 
 function addDataToDOM(data) {
 
@@ -18,33 +24,41 @@ function addDataToDOM(data) {
             if(window.location.href.includes("xbox")) {
                 if(post.data.title.toLowerCase().includes("[xbox]") && post.data.title.toLowerCase().includes(searchString.toLowerCase())) {
                     document.getElementById('posts').innerHTML += '<div class="col-xs-12 col-sm-6 col-md-3 post"> <h5> <a href = ' +post.data.url + '>' +post.data.title + '</a> </h5> <h6> <a href = "https://www.reddit.com/u/' + post.data.author + '"><i>' +post.data.author + '</i></a> </h6> <a href="https://www.reddit.com/message/compose/?to=' + post.data.author+ '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-send"></span> Message </button> </a> <a href="' + post.data.url + '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-comment"></span> Comment </button> </a> </div>';
+                    $('#xboxTab').addClass('active');
                 }
             } else if(window.location.href.includes("ps4") && post.data.title.toLowerCase().includes(searchString.toLowerCase())) {
                 if(post.data.title.toLowerCase().includes("[ps4]")) {
                     document.getElementById('posts').innerHTML += '<div class="col-xs-12 col-sm-6 col-md-3 post"> <h5> <a href = ' +post.data.url + '>' +post.data.title + '</a> </h5> <h6> <a href = "https://www.reddit.com/u/' + post.data.author + '"><i>' +post.data.author + '</i></a> </h6> <a href="https://www.reddit.com/message/compose/?to=' + post.data.author+ '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-send"></span> Message </button> </a> <a href="' + post.data.url + '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-comment"></span> Comment </button> </a> </div>';
+                    $('#ps4Tab').addClass('active');
                 }
             } else if(window.location.href.includes("pc") && post.data.title.toLowerCase().includes(searchString.toLowerCase())) {
                 if(post.data.title.toLowerCase().includes("[pc]")) {
                     document.getElementById('posts').innerHTML += '<div class="col-xs-12 col-sm-6 col-md-3 post"> <h5> <a href = ' +post.data.url + '>' +post.data.title + '</a> </h5> <h6> <a href = "https://www.reddit.com/u/' + post.data.author + '"><i>' +post.data.author + '</i></a> </h6> <a href="https://www.reddit.com/message/compose/?to=' + post.data.author+ '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-send"></span> Message </button> </a> <a href="' + post.data.url + '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-comment"></span> Comment </button> </a> </div>';
+                    $('#pcTab').addClass('active');
                 }
             } else if(window.location.href.includes("all") && post.data.title.toLowerCase().includes(searchString.toLowerCase())) {
                 document.getElementById('posts').innerHTML += '<div class="col-xs-12 col-sm-6 col-md-3 post"> <h5> <a href = ' +post.data.url + '>' +post.data.title + '</a> </h5> <h6> <a href = "https://www.reddit.com/u/' + post.data.author + '"><i>' +post.data.author + '</i></a> </h6> <a href="https://www.reddit.com/message/compose/?to=' + post.data.author+ '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-send"></span> Message </button> </a> <a href="' + post.data.url + '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-comment"></span> Comment </button> </a> </div>';
+                $('#allTab').addClass('active');
             }
         }   else {
             if(window.location.href.includes("xbox")) {
                 if(post.data.title.toLowerCase().includes("[xbox]")) {
                     document.getElementById('posts').innerHTML += '<div class="col-xs-12 col-sm-6 col-md-3 post"> <h5> <a href = ' +post.data.url + '>' +post.data.title + '</a> </h5> <h6> <a href = "https://www.reddit.com/u/' + post.data.author + '"><i>' +post.data.author + '</i></a> </h6> <a href="https://www.reddit.com/message/compose/?to=' + post.data.author+ '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-send"></span> Message </button> </a> <a href="' + post.data.url + '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-comment"></span> Comment </button> </a> </div>';
+                    $('#xboxTab').addClass('active');
                 }
             } else if(window.location.href.includes("ps4")) {
                 if(post.data.title.toLowerCase().includes("[ps4]")) {
                     document.getElementById('posts').innerHTML += '<div class="col-xs-12 col-sm-6 col-md-3 post"> <h5> <a href = ' +post.data.url + '>' +post.data.title + '</a> </h5> <h6> <a href = "https://www.reddit.com/u/' + post.data.author + '"><i>' +post.data.author + '</i></a> </h6> <a href="https://www.reddit.com/message/compose/?to=' + post.data.author+ '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-send"></span> Message </button> </a> <a href="' + post.data.url + '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-comment"></span> Comment </button> </a> </div>';
+                    $('#ps4Tab').addClass('active');
                 }
             } else if(window.location.href.includes("pc")) {
                 if(post.data.title.toLowerCase().includes("[pc]")) {
                     document.getElementById('posts').innerHTML += '<div class="col-xs-12 col-sm-6 col-md-3 post"> <h5> <a href = ' +post.data.url + '>' +post.data.title + '</a> </h5> <h6> <a href = "https://www.reddit.com/u/' + post.data.author + '"><i>' +post.data.author + '</i></a> </h6> <a href="https://www.reddit.com/message/compose/?to=' + post.data.author+ '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-send"></span> Message </button> </a> <a href="' + post.data.url + '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-comment"></span> Comment </button> </a> </div>';
+                    $('#pcTab').addClass('active');
                 }
             } else if(window.location.href.includes("all")) {
                 document.getElementById('posts').innerHTML += '<div class="col-xs-12 col-sm-6 col-md-3 post"> <h5> <a href = ' +post.data.url + '>' +post.data.title + '</a> </h5> <h6> <a href = "https://www.reddit.com/u/' + post.data.author + '"><i>' +post.data.author + '</i></a> </h6> <a href="https://www.reddit.com/message/compose/?to=' + post.data.author+ '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-send"></span> Message </button> </a> <a href="' + post.data.url + '"> <button type="button" class="btn btn-default halfwide"> <span class="glyphicon glyphicon-comment"></span> Comment </button> </a> </div>';
+                $('#allTab').addClass('active');
             }
         }
     });
